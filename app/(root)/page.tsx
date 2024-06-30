8f1b0f44422af57289f30aaf5e413888a20f1e79
RachaelBamigboye <RachaelBamigboye@users.noreply.github.com>
2024-06-30 19:03:10 +0100

    implementing chart and components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,30 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
+  //creating a new variable
+  const loggedIn = { firstname: 'Michael' }
   return (
-    <div>
-      Home
-    </div>
+    <section className="home">
+      <div className='home-content'>
+        <header className='home-header'>
+        <HeaderBox
+        //passing props
+        type="greeting"
+        title="Welcome"
+        user={loggedIn?.firstname || 'Guest'}
+        subtext='Access and manage your account and transaction efficiently.'
+        />
+
+        <TotalBalanceBox
+        accounts={[]}
+        totalBanks={1}
+        totalCurrentBalance={2350.60}
+        />
+      </header>
+      </div>
+    </section>
   )
 }
 
---
+++ components/TotalBalanceBox.tsx
@@ -0,0 +1,34 @@
+import AnimatedCounter from './AnimatedCounter';
+import DoughnutChart from './DoughnutChart';
+
+
+const TotalBalanceBox = ({
+    accounts = [], totalBanks, totalCurrentBalance
+}: TotlaBalanceBoxProps)=> {
+  
+  return (
+    <section className="total-balance">
+      <div className='total-balance-chart'>
+        {/* Doughnutchart using chart.js*/}
+        <DoughnutChart accounts={accounts} />
+        </div>
+
+        <div className='flex flex-col gap-6'>
+            <h2>Bank Account: {totalBanks}
+            </h2>
+            <div className='flex flex-col gap-2'>
+<p className='total-balance-label'>Total Current Balance</p>
+<p className='total-balance-amount flex-center gap-2'></p>
+            <p className= 'total-balance-amount flex-center gap-2'>
+               <AnimatedCounter amount=
+               {totalCurrentBalance} />
+            
+            </p>
+            </div>
+
+        </div>
+    </section> 
+  )
+}
+
+export default TotalBalanceBox
